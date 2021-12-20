cfed5d29f16c3d890864e671a6fe7ebf11237e08
Varun S Athreya <varun.sathreya@gmail.com>
2021-12-20 19:50:48 +0530

    Updated container

--- app/lib/components/gradient_container.dart
+++ app/lib/components/gradient_container.dart
@@ -7,19 +7,31 @@ class GradientContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Theme.of(context).primaryColor,
-            Theme.of(context).colorScheme.secondary,
-          ],
-        ),
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Theme.of(context).colorScheme.secondary,
+                      Theme.of(context).backgroundColor,
+                    ],
+                  ),
+                ),
+                alignment: Alignment.center,
+                width: double.infinity,
+                child: child,
+              ),
+            ),
+          );
+        },
       ),
-      alignment: Alignment.center,
-      child: child,
     );
   }
 }
