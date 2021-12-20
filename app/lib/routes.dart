fd034d9d9f302094a9c79fc919682a909cac55cf
Varun S Athreya <varun.sathreya@gmail.com>
2021-12-21 01:15:21 +0530

    Added survey page

--- app/lib/routes.dart
+++ app/lib/routes.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'screens/home.dart';
 import 'screens/sign_in.dart';
 import 'screens/sign_up.dart';
+import 'screens/survey.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SignIn.routeName: (context) => const SignIn(),
   SignUp.routeName: (context) => const SignUp(),
   Home.routeName: (context) => const Home(),
+  Survey.routeName: (context) => const Survey(),
 };
