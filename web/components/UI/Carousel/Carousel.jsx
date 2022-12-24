eb3140c13e8bcff2949e7d4fe5b184daeca1b60e
Varun S Athreya <varun.sathreya@gmail.com>
2022-12-24 23:23:39 +0530

    Fixed build issue

--- web/components/UI/Carousel/Carousel.jsx
+++ web/components/UI/Carousel/Carousel.jsx
@@ -59,7 +59,6 @@ const Carousel = () => {
             <link
                 rel="stylesheet"
                 type="text/css"
-                charSet="UTF-8"
                 href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
             />
             <link
